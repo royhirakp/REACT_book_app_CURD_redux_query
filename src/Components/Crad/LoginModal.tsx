2a837f5efca518f8c874d595bf33e9b87e0c51ae
Hirak Roy <royhirakp@gmail.com>
2024-02-19 00:51:53 +0530

    add test email

--- src/Components/Crad/LoginModal.tsx
+++ src/Components/Crad/LoginModal.tsx
@@ -109,6 +109,10 @@ export default function LoginModal({
                 </Typography>
               </Box>
 
+              <Typography>
+                to test login use this email
+                <br /> email: "12@12.com" password: "12@12.com"
+              </Typography>
               <Stack direction="row" justifyContent="center">
                 <Button
                   onClick={async () => {
